587689f41aa7aa09bcf5b64b4ad03315acfc9aeb
Tim Goyer <tim.goyer@gmail.com>
2025-01-01 23:33:12 -0600

    WIP

--- fospos-api.client/src/views/Pos/index.jsx
+++ fospos-api.client/src/views/Pos/index.jsx
@@ -1,34 +1,34 @@
+import { lightTheme, ThemeProvider } from "@/utilities/Theme/Theme";
+import { Button } from "@/components";
+
 import "./styles.css";
-//import { Button } from "@/components";
 
 const Pos = () => {
   return (
-    <div className="page">
-      <div className="header">AJ's Bar and Grill</div>
+    <ThemeProvider theme={lightTheme}>
+      <div className="page">
+        <div className="header">AJ's Bar and Grill</div>
 
-      <div className="main">
-        <div className="subCategoryContainer">
-          
-        </div>
-      
-        <div className="itemListContainer"></div>
+        <div className="main">
+          <div className="subCategoryContainer"></div>
 
-        <div className="cartContainer">
-          <div className="cartTitle">Cart</div>
+          <div className="itemListContainer"></div>
 
-          <div className="cartList">
-             
-          </div>
+          <div className="cartContainer">
+            <div className="cartTitle">Cart</div>
 
-          <div className="cartQuantity">
-            <button>-</button>
-            <input value="1" style={{textAlign: "center"}} />
-            <button>+</button>
+            <div className="cartList"></div>
+
+            <div className="cartQuantity">
+              <Button>-</Button>
+              <input value="1" style={{ textAlign: "center" }} />
+              <Button>+</Button>
+            </div>
           </div>
         </div>
+        <div className="customer">customer</div>
       </div>
-      <div className="customer">customer</div>
-    </div>
+    </ThemeProvider>
   );
 };
 
