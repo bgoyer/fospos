c3b39a18c476d233bc96c116fdd1bdbad24aff0f
Tim Goyer <tim.goyer@gmail.com>
2025-01-07 18:09:52 -0600

    WIP

--- fospos-api.client/src/views/Pos/index.jsx
+++ fospos-api.client/src/views/Pos/index.jsx
@@ -5,53 +5,54 @@ import "./styles.css";
 import { useEffect, useState } from "react";
 
 const Pos = () => {
-
   const [qty, setQty] = useState(1);
 
   const handleAddButtonClick = () => {
-    setQty(current => current+1);
-  }
+    setQty((current) => current + 1);
+  };
 
   const handleMinusButtonClick = () => {
-    setQty(current => current === 1 ? 1 : current-1);
-  }
+    setQty((current) => (current === 1 ? 1 : current - 1));
+  };
   const handleQtyChange = (evt) => {
     const value = Number(evt.currentTarget.value);
     setQty(value);
-  }
+  };
   const verifyQtyChange = () => {
     setQty(qty > 0 ? qty : 1);
-  }
+  };
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
   const [subcategories, setSubcategories] = useState([]);
 
   useEffect(() => {
-      const go = async () => {
-          const response = await fetch('/api/Products');
-          const data = await response.json();
-          setProducts(data);
-      }
-      go();
-  }, [])
+    const go = async () => {
+      const response = await fetch("/api/Products");
+      const data = await response.json();
+      setProducts(data);
+    };
+    go();
+  }, []);
   useEffect(() => {
     const go = async () => {
-        const response = await fetch('/api/Subcategories');
-        const data = await response.json();
-        setSubcategories(data);
-    }
+      const response = await fetch("/api/Subcategories");
+      const data = await response.json();
+      setSubcategories(data);
+    };
     go();
-  }, [])
+  }, []);
   useEffect(() => {
     const go = async () => {
-        const response = await fetch('/api/Categories');
-        const data = await response.json();
-        setCategories(data);
-    }
+      const response = await fetch("/api/Categories");
+      const data = await response.json();
+      setCategories(data);
+    };
     go();
-  }, [])
-  const cartList = {}
-  const subcategoryItems = subcategories.map(item => <button>{item.name}</button>)
+  }, []);
+  const cartList = {};
+  const subcategoryItems = subcategories.map((item) => (
+    <button>{item.name}</button>
+  ));
 
   return (
     <ThemeProvider theme={lightTheme}>
@@ -68,12 +69,17 @@ const Pos = () => {
           <div className="cartContainer">
             <div className="cartTitle">Cart</div>
 
-            <div className="cartList">
-            </div>
+            <div className="cartList"></div>
 
             <div className="cartQuantity">
               <Button onClick={handleMinusButtonClick}>-</Button>
-              <input type="number" value={qty} onBlur={verifyQtyChange} onChange={handleQtyChange} style={{ textAlign: "center" }} />
+              <input
+                type="number"
+                value={qty}
+                onBlur={verifyQtyChange}
+                onChange={handleQtyChange}
+                style={{ textAlign: "center" }}
+              />
               <Button onClick={handleAddButtonClick}>+</Button>
             </div>
           </div>
